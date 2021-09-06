2e6894b522b71a9be2b8ee41cb55612aff354f49
Axel Gourlin <gourlin.axel@gmail.com>
2021-09-06 10:26:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+// let width = document.body;//.innerWidth();
+// console.log(width);
+
+// let bodywidth = document.querySelector(".mybody");//.style.width = width;
+// console.log(bodywidth);
+
+
+// let height = document.querySelector(".mybody");//.innerHeight();
+
+// let bodyheight = document.querySelector(".mybody");//.style.height = height;
+// console.log(height, bodyheight);
